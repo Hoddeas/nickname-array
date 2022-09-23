181e9b4551ad80bb4e996217b6c84edbb54e7d2c
Hoddeas <fakehoddeas@gmail.com>
2022-09-23 12:35:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ let nickname = ["Destroyer", "Squid", "Untouchable", "The GOAT", "All-Knowing", 
 // Event Listener for One Nickname
 document.getElementById("randomNickname").addEventListener("click", function() {
     let firstName = document.getElementById("first-name").value;
-    let lastName = document.getElementById("last-name").value;
-    document.getElementById("outputNicknames").innerHTML = firstName + ' "' + nickname[randomInt(0, nickname.length)] + '" ' + lastName;
+    let lastName = document.getElementById("last-name").value; 
+    if (firstName != "" && lastName != "") {
+        document.getElementById("outputNicknames").innerHTML = firstName + ' "' + nickname[randomInt(0, nickname.length)] + '" ' + lastName;
+    } else {
+        document.getElementById("outputNicknames").innerHTML = "Please fill empty boxes.";
+    }
 });
 
 // Event Listener for All Nicknames
@@ -20,8 +24,11 @@ document.getElementById("allNicknames").addEventListener("click", function() {
     let firstName = document.getElementById("first-name").value;
     let lastName = document.getElementById("last-name").value;
     document.getElementById("outputNicknames").innerHTML = "";
-    for (let i = 0; i < nickname.length; i++) {
-    document.getElementById("outputNicknames").innerHTML += firstName + ' "' + nickname[i] + '" ' + lastName + "<br>";
+    if (firstName != "" && lastName != "") {
+        for (let i = 0; i < nickname.length; i++) {
+            document.getElementById("outputNicknames").innerHTML += firstName + ' "' + nickname[i] + '" ' + lastName + "<br>";
+            }
+    } else {
+        document.getElementById("outputNicknames").innerHTML = "Please fill empty boxes.";
     }
 });
-
